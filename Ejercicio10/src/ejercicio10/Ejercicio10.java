77ccfc34a4b6dab06e63d9e2425072db1bd1a793
Antonio ojeda <ajojeda6@utpl.edu.ec>
2024-10-24 12:11:20 -0500

    Trabajo final

--- Ejercicio10/src/ejercicio10/Ejercicio10.java
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -14,6 +14,15 @@ public class Ejercicio10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int a = 25;
+        int b = 10;
+        int c = 100;
+        boolean d = true;
+        boolean e = false;
+        int f = 5;
+        int g = 2;
+        boolean resultado = ((Math.sqrt(a)*b)>= c )&&(d)|| (e)||((b/f)>= g);
+        System.out.println(resultado);
         // TODO code application logic here
     }
     
