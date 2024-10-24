b9e58247ab2a3095a3340e980e75664221682d62
Juan Villamagua <ajojeda6@utpl.edu.ec>
2024-10-24 11:44:44 -0500

    nuevos cambios

---
+++ Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio06;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 81;
+        int b = 9;
+        int c = 3;
+        int d = 10;
+        int e = 1;
+        boolean resultado = (((Math.sqrt(a)+ b)/c) == b)&&(d>e);
+        System.out.println(resultado);
+          
+        // TODO code application logic here
+    }
+    
+}
---
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio07;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 81;
+        int b = 9;
+        int c = 3;
+        int d = 10;
+        int e = 1;
+        int f = 100;
+        int g = 25;
+        boolean resultado = (((Math.sqrt(a)+ b)/c) == b)||(d>e) && ((f/g)>= Math.sqrt(f));
+        System.out.println(resultado);
+        // TODO code application logic here
+    }
+    
+}
